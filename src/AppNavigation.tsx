04ed34c5044a48bcd10785614efba667f4491d9e
Melvin PERSONNIER <melvin.personnier@gmail.com>
2022-09-14 13:39:28 +0200

    Initializing navigation

---
+++ src/AppNavigation.tsx
@@ -0,0 +1,106 @@
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { useQuery } from '@apollo/client';
+import { GET_THEME } from './graphql/reactivities/ThemeVariable';
+import i18n from './utils/i18n';
+import { SplashScreen, LookupScreen, QRScanScreen } from './screens';
+
+const LookupStack = createStackNavigator();
+function LookupStackScreen(theme: any) {
+  return () => (
+    <LookupStack.Navigator>
+      <LookupStack.Screen
+        name="LookupStack"
+        component={LookupScreen}
+        options={{ headerShown: false }}
+      />
+    </LookupStack.Navigator>
+  );
+}
+
+const QRScanStack = createStackNavigator();
+function QRScanStackScreen(theme: any) {
+  return () => (
+    <QRScanStack.Navigator>
+      <QRScanStack.Screen
+        name="QRScanStack"
+        component={QRScanScreen}
+        options={{ headerShown: false }}
+      />
+    </QRScanStack.Navigator>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+export default function AppNavigation() {
+
+  const _theme = useQuery(GET_THEME);
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  // SplashScreen
+  if (isLoading) {
+    return <SplashScreen />;
+  }
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName={'Lookup'}
+        screenOptions={{
+          tabBarActiveTintColor: _theme.data.theme.textColorPrimary,
+          tabBarInactiveTintColor: _theme.data.theme.textColorSecondary,
+          tabBarShowLabel: true,
+          tabBarStyle: { backgroundColor: _theme.data.theme.bgColorThird },
+        }}
+      >
+        <Tab.Group
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: _theme.data.theme.bgColorThird
+            },
+            headerTintColor: _theme.data.theme.textColorPrimary,
+            tabBarLabelStyle: styles.tabBarLabelStyle,
+            tabBarIconStyle: styles.tabBarIconStyle,
+            tabBarLabelPosition: 'beside-icon',
+          }}
+        >
+          <Tab.Screen
+            name="Lookup"
+            component={LookupStackScreen(_theme.data.theme)}
+            options={{
+              title: i18n.t('Header.Lookup'),
+              tabBarLabel: i18n.t('BottomTab.Lookup'),
+            }}
+          />
+          <Tab.Screen
+            name="QRScan"
+            component={QRScanStackScreen(_theme.data.theme)}
+            options={{
+              title: i18n.t('Header.QRScan'),
+              tabBarLabel: i18n.t('BottomTab.QRScan'),
+            }}
+          />
+        </Tab.Group>
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  tabBarLabelStyle: {
+    fontSize: 15,
+  },
+  tabBarIconStyle: {
+    display: 'none',
+  }
+})
